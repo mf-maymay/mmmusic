1921fdba65b23febdf20f0dcd5646db54cbaa25a
14malbright <14malbright@gmail.com>
2020-02-05 00:12:31 -0500

    Merge plotting functions into artist_finder.py

--- artist_finder.py
+++ artist_finder.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from functools import reduce
 from itertools import combinations
+import os
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
 import networkx as nx
-from artists import get_artist, get_related
+from artists import get_artist, get_artist_name, get_related
 
 
 class Finder(object):
@@ -103,13 +106,93 @@ class Finder(object):
 
         print("Nodes (post-paths-trim):", len(self.G_cut.nodes))
 
+    def plot(self,
+             node_color_near="#6177aa",
+             node_color_far="#0e1b3a",
+             edge_color="#000102",
+             font_color="#b9cdfb",
+             fig_color="#2e4272",
+             save=False,
+             **plot_kwargs  # passed to nx.draw
+             ):
+
+        if not finder.is_grown:
+            raise RuntimeError("finder.grow() must be called before plot()")
+
+        dist = {artist_id: 0 for artist_id in finder.artists}
+
+        if finder.paths:
+            pres = []
+            dists = []
+
+            for i, artist_id in enumerate(self.artists):
+                pres.append(nx.bfs_predecessors(self.G_cut, artist_id))
+                dists.append({artist_id: 0})
+
+            for i, artist_id in enumerate(self.artists):
+                for artist, pre in pres[i]:
+                    dists[i][artist] = dists[i][pre] + 1
+
+            for artist_id in self.G_cut.nodes:
+                dist[artist_id] = min(map(lambda d: d.get(artist_id, 1),
+                                          dists))
+
+        for artist_id in self.G_cut.nodes:
+            if artist_id not in dist:
+                dist[artist_id] = 1
+
+        max_dist = max(max(dist.values()), 1)
+
+        color = {k: dist[k] / max_dist for k in self.G_cut.nodes}
+
+        node_labels = {artist_id:
+                       get_artist_name(artist_id).replace(r"$", r"\$")
+                       for artist_id in self.G_cut.nodes}
+
+        cmap = LinearSegmentedColormap.from_list("music",
+                                                 [node_color_near,
+                                                  node_color_far])
+
+        fig, ax = plt.subplots(figsize=(16, 9))
+
+        nx.draw_kamada_kawai(self.G_cut,
+                             with_labels=True,
+                             ax=ax,
+                             cmap=cmap,
+                             node_color=[color[k] for k in self.G_cut.nodes],
+                             edge_color=edge_color,
+                             font_color=font_color,
+                             labels=node_labels,
+                             **plot_kwargs
+                             )
+
+        fig.set_facecolor(fig_color)
+
+        fig.tight_layout()
+
+        if save:
+            os.makedirs("output", exist_ok=True)
+            fig.savefig("output/" +
+                        "-".join(a.id for a in self.artists) +
+                        ".png",
+                        facecolor=fig_color)
+
+        return fig, ax
+
+    def grow_and_plot(self, **plot_kw):
+        self.grow()
+        self.trim()
+        return self.plot(**plot_kw)
+
 
 if __name__ == "__main__":
     from artist_ids import ids
 
-    finder = Finder(ids["death grips"], ids["earl sweatshirt"])
+    finder = Finder(ids["david bowie"], ids["faust"])
 
-    finder.grow()
+    finder.grow_and_plot()
+
+    print()
 
     for midpoint in sorted(finder.midpoints()):
         print(midpoint)
