7d6ea00734fbe56c249ff7145bf81eb51554e07c
14malbright <14malbright@gmail.com>
2020-09-04 20:40:55 -0400

    Cache Album objects
    Album objects can now be constructed by `Album(id)` or as before by
    `Album(id, name, artists)`.

--- albums.py
+++ albums.py
@@ -1,11 +1,36 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from cache import Cache
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
 
 _Album = namedtuple("_Album", ("id", "name", "artist_ids"))
 
 
 class Album(_Album):
     __slots__ = ()
+
+    @Cache()
+    def __new__(cls, *args):
+        if len(args) == 1:
+            album_id = args[0]
+
+            if isinstance(album_id, Album):
+                return album_id  # Album(Album(x)) == Album(x)
+
+            album = sp.album(album_id)
+
+            return super().__new__(cls,
+                                   album_id,
+                                   album["name"],
+                                   tuple(artist["id"]
+                                         for artist in album["artists"])
+                                   )
+
+        return super().__new__(cls, *args)
 
     def __eq__(self, other):
         return (hash(self) == hash(other))
