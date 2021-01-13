f2416ee0af8768fae4314a790729d22c7bacaaf2
14malbright <14malbright@gmail.com>
2021-01-12 19:58:22 -0500

    Use functools.wraps. Add record_calls decorator

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from functools import wraps
 import requests
 
 
 def no_timeout(func):
+    @wraps(func)
     def wrapped(*args, **kwargs):
         while True:
             try:
@@ -13,6 +15,17 @@ def no_timeout(func):
             except requests.ReadTimeout:
                 print(f"ReadTimout on {func.__name__}. Retrying...")
                 continue
+    return wrapped
+
+
+def record_calls(func):
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        args_list = ", ".join([repr(arg) for arg in args] +
+                              [f"{kw}={repr(arg)}"
+                               for kw, arg in kwargs.items()])
+        print(f"{func.__name__}({args_list})")
+        return func(*args, **kwargs)
     return wrapped
 
 
