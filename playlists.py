79df08ec9f51b792ab8a3034fae4c4529cf24c84
Albright <mason.d.albright@accenture.com>
2021-04-13 23:33:36 -0400

    Move create_playlist

--- playlists.py
+++ playlists.py
@@ -7,6 +7,25 @@ class Playlist(object):
         self.artists_to_exclude = set(artists_to_exclude)
 
 
+def create_playlist(user, tracks, playlist_name, description="", confirm=True):
+    if (not confirm or
+            input(f"Create playlist '{playlist_name}'? (y/n): ")[0] in "yY"):
+        print(f"Creating '{playlist_name}'...")
+
+        user.setup_sp(scope="playlist-modify-private")  # XXX
+
+        playlist = user.sp.user_playlist_create(user._username,
+                                                playlist_name,
+                                                public=False,
+                                                description=description)
+
+        for i in range(0, (l := len(tracks)) // 100 + bool(l % 100)):
+            to_add = tracks[(100 * i):(100 * (i + 1))]
+            user.sp.user_playlist_add_tracks(user._username,
+                                             playlist["id"],
+                                             to_add)
+
+
 playlists = {
     "american": Playlist("american", pattern=".*(americana|country|cow).*"),
     "black": Playlist("black", pattern=".*black.*"),
--- utils.py
+++ utils.py
@@ -29,25 +29,6 @@ def record_calls(func):
     return wrapped
 
 
-def create_playlist(user, tracks, playlist_name, description="", confirm=True):
-    if (not confirm or
-            input(f"Create playlist '{playlist_name}'? (y/n): ")[0] in "yY"):
-        print(f"Creating '{playlist_name}'...")
-
-        user.setup_sp(scope="playlist-modify-private")  # XXX
-
-        playlist = user.sp.user_playlist_create(user._username,
-                                                playlist_name,
-                                                public=False,
-                                                description=description)
-
-        for i in range(0, (l := len(tracks)) // 100 + bool(l % 100)):
-            to_add = tracks[(100 * i):(100 * (i + 1))]
-            user.sp.user_playlist_add_tracks(user._username,
-                                             playlist["id"],
-                                             to_add)
-
-
 def get_audio_features(user, tracks):
     out = []
     for i in range(len(tracks) // 100 + bool(len(tracks) % 100)):
