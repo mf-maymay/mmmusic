b7f451f390f18c409f32ee82467eac8adc88a727
Albright <mason.d.albright@accenture.com>
2021-05-22 15:59:01 -0400

    Add cliques()

--- genre_graph.py
+++ genre_graph.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import networkx as nx
+from networkx.algorithms.approximation import clique_removal
 from genres import genre_overlaps, genres_and_members
 
 
@@ -20,15 +21,15 @@ def genre_map(size_min, artists, draw=False):
     return graph
 
 
-if __name__ == "__main__":
-    from networkx.algorithms.approximation import clique_removal
-    from user import User
+def cliques(artists=None, graph=None):
+    if graph is None:
+        if artists is None:
+            raise ValueError("artists and graph cannot both be None")
+        else:
+            graph = genre_map(0, artists)
 
-    user = User(input("username: "))
-
-    artists = user.artists()
-
-    graph = genre_map(20, artists, draw=True)
+    if artists is None:
+        artists = set(graph.nodes)
 
     largest_independent_set, maximal_cliques = clique_removal(
         genre_map(0, artists)
@@ -47,7 +48,24 @@ if __name__ == "__main__":
         key=lambda c: (-sum(len(genre_artists[g]) for g in c[1]), c[0])
     )
 
-    print("\n".join(f"{rep}:\n\t" +
-                    "\n\t".join(f"{c} ({len(genre_artists[c])})"
-                                for c in clique)
-                    for rep, clique in groups_list))
+    return groups_list  # XXX
+
+
+if __name__ == "__main__":
+    from user import User
+
+    user = User(input("username: "))
+
+    artists = user.artists()
+
+    genre_map(20, artists, draw=True)
+
+    groups_list = cliques(artists=artists)
+
+    genre_artists = genres_and_members(artists)
+
+    for rep, clique in groups_list:
+        clique_sizes = [len(genre_artists[genre]) for genre in clique]
+        print(f"{rep} ({sum(clique_sizes)}):")
+        for genre, size in zip(clique, clique_sizes):
+            print(f"\t{genre} ({size})")
