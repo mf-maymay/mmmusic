277ea619a68a009bb68022165351b3c79b97c172
Albright <mason.d.albright@accenture.com>
2021-06-06 15:50:39 -0400

    Clean playlist_utils.py

--- playlist_utils.py
+++ playlist_utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from genres import artists_of_genres_matching, genres_matching
+from genres import artists_of_genres_matching
 from track import Track
 
 
@@ -32,24 +32,13 @@ def tracks_by_audio_feature(features_filter_func, base=None):  # XXX
     return get_tracks
 
 
-def tracks_by_genre_pattern(pattern, artists_to_exclude=(), display=True):
+def tracks_by_genre_pattern(pattern, artists_to_exclude=()):
     def get_tracks(user):
         artists = (
             artists_of_genres_matching(pattern, user.artists())
             - set(artists_to_exclude)
         )
-
-        if display:
-            print(f"'{pattern}' genres:")
-            for genre in sorted(genres_matching(pattern, artists)):
-                print("*", genre)
-            print()
-            print(f"'{pattern}' artists:")
-            for artist in sorted(artists):
-                print("*", artist)
-
         albums = albums_from_artists(user, artists)
-
         return tracks_from_albums(albums)
 
     get_tracks.__doc__ = f"tracks_by_genre_pattern({repr(pattern)})"
@@ -77,21 +66,3 @@ def tracks_from_playlist(playlist_id):
 
     get_tracks.__doc__ = f"tracks_from_playlist({repr(playlist_id)})"
     return get_tracks
-
-
-if __name__ == "__main__":
-    from artist import Artist
-    from user import User
-
-    artists = [Artist("4Z8W4fKeB5YxbusRsdQVPb"),
-               Artist("0oSGxfWSnnOXhD2fKuz2Gy")]
-
-    user = User(input("username: "))
-
-    albums = albums_from_artists(user, artists)
-
-    tracks = tracks_from_albums(albums)
-
-    # -------
-
-    all_tracks = all_user_tracks(user)
