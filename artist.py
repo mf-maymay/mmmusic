49376fb2f6742544fc91880dbaf652d6037ba8ae
Albright <mason.d.albright@accenture.com>
2021-08-01 17:27:25 -0400

    Set Artist to inherit from SpotifyObjectBase

--- artist.py
+++ artist.py
@@ -1,67 +1,33 @@
 # -*- coding: utf-8 -*-
-from collections import namedtuple
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 from cache import Cache
+from base_class import SpotifyObjectBase
+from utils import no_timeout
 
 
-_Artist = namedtuple("_Artist", ("id", "name", "genres", "popularity"))
+class Artist(SpotifyObjectBase):
+    FIELDS = ("id", "name", "genres", "popularity")
+    __slots__ = (*FIELDS, "info")
 
+    def __init__(self, artist_id=None, *, info=None):
+        super().__init__(id=artist_id, info=info)
 
-class Artist(_Artist):
-    __slots__ = ()
+        self.genres = tuple(sorted(self.info["genres"]))
 
-    _sp = spotipy.Spotify(
-        client_credentials_manager=SpotifyClientCredentials()
-    )
+        self.popularity = self.info["popularity"]
 
-    _use_name_for_repr = False  # Use to replace namedtuple's repr with name.
-
-    @Cache(key_func=lambda *args, **kwargs:  # Let kwargs fail in __new__.
-           args[1] if len(args) == 2 else args[1:])
-    def __new__(cls, artist_id, *args):
-        if not args:
-            if isinstance(artist_id, Artist):
-                return artist_id  # Artist(Artist(x)) == Artist(x)
-
-            artist = cls._sp.artist(artist_id)
-
-            return super().__new__(cls,
-                                   artist_id,
-                                   artist["name"],
-                                   tuple(sorted(artist["genres"])),
-                                   artist["popularity"])
-
-        return super().__new__(cls, artist_id, *args)
+    @classmethod
+    @no_timeout
+    def full_response(cls, artist_id):
+        return cls._sp.artist(
+            artist_id if not isinstance(artist_id, cls) else artist_id.id
+        )
 
     @Cache()
     def related(self):
-        return set(Artist(a["id"])
-                   for a in self._sp.artist_related_artists(self.id)["artists"]
-                   )
-
-    @classmethod
-    def clear(cls):
-        cls.__new__.clear()
-
-    def __eq__(self, other):
-        return hash(self) == hash(other)
-
-    def __hash__(self):
-        return hash(self.id)
-
-    def __lt__(self, other):
-        return self.name < str(other)
-
-    def __repr__(self):
-        return self.name if self._use_name_for_repr else super().__repr__()
-
-    def __str__(self):
-        return self.name
+        return set(
+            Artist(a["id"]) for a in self._sp.artist_related_artists(self.id)["artists"]
+        )
 
 
 if __name__ == "__main__":
-    artists = [
-        Artist("0oSGxfWSnnOXhD2fKuz2Gy"),  # David Bowie
-        Artist("id", "name", "genres", "popularity")
-    ]
+    artist = Artist("0oSGxfWSnnOXhD2fKuz2Gy")
