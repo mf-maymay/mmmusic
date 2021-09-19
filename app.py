8605819c5623f17dc747c4d6f90c9bbeea532262
Albright <mason.d.albright@accenture.com>
2021-09-19 00:44:21 -0400

    Add artist finder utility to web app

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from pathlib import Path
 
+from flask import Flask, render_template, request, send_file
+
+from artist_finder import grow_and_plot
 from playlist_utils import shuffle_playlist
 from user import User
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=("GET", "POST"))
-def hello_world():
+@app.route("/finder/<path:varargs>")
+def finder_pager(varargs=None):
+    if varargs is None:
+        return ""
+    seeds = sorted(varargs.split("/"))
+    file = Path("output") / ("-".join(seeds) + ".png")
+    if not file.is_file():
+        graph, plot = grow_and_plot(*seeds, save=True)  # XXX
+    return send_file(file)
+
+
+@app.route("/shuffle", methods=("GET", "POST"))
+def shuffle_page():
     if request.method == "POST":
         username = request.form["username"]
         playlist_id = request.form["playlist_id"]
