d79216910549946e21006687e9233ce8e79ec24e
Albright <mason.d.albright@accenture.com>
2021-10-15 17:52:39 -0400

    Update web app to properly share images online

--- app.py
+++ app.py
@@ -2,23 +2,30 @@
 from pathlib import Path
 
 from flask import Flask, render_template, request, send_file
+import matplotlib
 
+from app_config import BASE_DIR
 from artist_finder import grow_and_plot
 from playlist_utils import shuffle_playlist
 from user import User
 
+matplotlib.use("Agg")
+
 app = Flask(__name__)
 
 
-@app.route("/finder/<path:varargs>")
-def finder_pager(varargs=None):
+@app.route("/connect-artists/<path:varargs>")
+def finder_output(varargs=None):
+    print(varargs)
     if varargs is None:
         return ""
     seeds = sorted(varargs.split("/"))
-    file = Path("output") / ("-".join(seeds) + ".png")
-    if not file.is_file():
-        graph, plot = grow_and_plot(*seeds, save=True)  # XXX
-    return send_file(file)
+    file = BASE_DIR / Path("output") / ("-".join(seeds) + ".png")
+    try:
+        return send_file(file)
+    except Exception:
+        graph, (fig, ax) = grow_and_plot(*seeds, save=file)  # XXX
+        return send_file(file)
 
 
 @app.route("/shuffle", methods=("GET", "POST"))
---
+++ app_config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+BASE_DIR = Path(".").absolute()
