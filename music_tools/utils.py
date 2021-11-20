cc759c8b8ab413c5c5501a3548e4fc4c80e2c30c
Albright <mason.d.albright@accenture.com>
2021-11-20 16:45:57 -0500

    Delete record_calls

--- music_tools/utils.py
+++ music_tools/utils.py
@@ -20,19 +20,6 @@ def no_timeout(func):
     return wrapped
 
 
-def record_calls(func):  # TODO: delete?
-    @wraps(func)
-    def wrapped(*args, **kwargs):
-        args_list = ", ".join(
-            [repr(arg) for arg in args]
-            + [f"{kw}={repr(arg)}" for kw, arg in kwargs.items()]
-        )
-        print(f"{func.__name__}({args_list})")
-        return func(*args, **kwargs)
-
-    return wrapped
-
-
 def take_x_at_a_time(sequence, x):
     quotient, remainder = divmod(len(sequence), x)
     for i in range(quotient + bool(remainder)):
