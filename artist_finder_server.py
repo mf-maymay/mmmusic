95057f66e7fc86603288fbdb9ca5ef8a41d982dc
Albright <mason.d.albright@accenture.com>
2022-01-10 00:00:13 -0500

    Connect artists concurrently with multithreading

--- artist_finder_server.py
+++ artist_finder_server.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from concurrent.futures import ThreadPoolExecutor  # XXX: threadsafe?
 from multiprocessing.connection import Listener
 from pathlib import Path
 import traceback
@@ -11,10 +12,13 @@ from music_tools.artist import Artist
 
 IMAGES_DIR = Path("output")
 
-workload = set()
+
+def connect_artists(artists, *, save):
+    grow_and_plot(*artists, save=save)
+    print("connected: ", ", ".join(artists))
 
 
-def return_artists(conn):
+def handle_connection(conn, *, executor, in_progress: dict):
     try:
         while True:
             artist_ids = conn.recv()
@@ -26,20 +30,23 @@ def return_artists(conn):
             file_path = (IMAGES_DIR / "-".join(artists)).with_suffix(".png")
 
             if file_path.is_file():
-                print("network exists already: ", ", ".join(artist_ids))
+                print("network exists already: ", ", ".join(artists))
                 conn.send(file_path)  # XXX
                 continue
 
+            if artists in in_progress:
+                if in_progress[artists].done():
+                    try:
+                        in_progress[artists].result()  # raise exceptions ?
+                    finally:
+                        del in_progress[artists]  # XXX
+
+            else:
+                in_progress[artists] = executor.submit(
+                    connect_artists, artists, save=file_path
+                )
+
             conn.send(None)  # XXX
-
-            if artists not in workload:
-                workload.add(artists)
-
-                grow_and_plot(*artists, save=file_path)
-
-                workload.remove(artists)
-
-                print("connected: ", ", ".join(artist_ids))
 
     except EOFError:
         print("Connection closed")
@@ -47,12 +54,17 @@ def return_artists(conn):
 
 def server(address):
     serv = Listener(address)
-    while True:
-        try:
-            client = serv.accept()
-            return_artists(client)
-        except Exception:
-            traceback.print_exc()
+
+    in_progress = {}  # artists -> future
+
+    with ThreadPoolExecutor() as executor:
+        while True:
+            try:
+                client = serv.accept()
+                handle_connection(client, executor=executor, in_progress=in_progress)
+
+            except Exception:
+                traceback.print_exc()
 
 
 if __name__ == "__main__":
