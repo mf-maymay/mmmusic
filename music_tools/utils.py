a0f7715f36feea16cb54c217b85eb1aedb091959
Albright <mason.d.albright@accenture.com>
2022-01-22 21:08:41 -0500

    Accept non-sequences for take_x_at_a_time()

--- music_tools/utils.py
+++ music_tools/utils.py
@@ -20,7 +20,8 @@ def no_timeout(func):
     return wrapped
 
 
-def take_x_at_a_time(sequence, x):
+def take_x_at_a_time(items, x):
+    sequence = list(items)
     quotient, remainder = divmod(len(sequence), x)
     for i in range(quotient + bool(remainder)):
         yield sequence[i * x : (i + 1) * x]
