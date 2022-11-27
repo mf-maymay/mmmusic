a237261d362222ce88a90049e2898cff41a8cfce
Mason Albright <14malbright@gmail.com>
2022-11-26 23:52:44 -0600

    Move spotipy code outside of artist.py

--- lib/models/artist.py
+++ lib/models/artist.py
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
-from functools import lru_cache
-
 from pydantic import BaseModel
 from requests.exceptions import HTTPError
-import spotipy
 from spotipy.exceptions import SpotifyException
-from spotipy.oauth2 import SpotifyClientCredentials
 
-from lib.utils import no_timeout
+from lib.models import spotify
 
 ArtistID = str
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(), retries=None)
-_get_artist_json = lru_cache(maxsize=None)(no_timeout(sp.artist))
-_get_related_artists_json = lru_cache(maxsize=None)(
-    no_timeout(sp.artist_related_artists)
-)
 
 
 class Artist(BaseModel):
@@ -38,7 +28,7 @@ def get_artist(artist_id: ArtistID | Artist) -> Artist:
     if isinstance(artist_id, Artist):
         return artist_id
 
-    artist_json = _get_artist_json(artist_id)
+    artist_json = spotify.get_artist(artist_id)
 
     return Artist.parse_obj(artist_json)
 
@@ -47,18 +37,17 @@ def get_related_artists(artist: ArtistID | Artist) -> tuple[Artist, ...]:
     if isinstance(artist, Artist):
         artist = artist.id
 
-    related_artists_json = _get_related_artists_json(artist)
+    related_artists_json = spotify.get_related_artists(artist)
 
-    return tuple(get_artist(artist["id"]) for artist in related_artists_json["artists"])
+    return tuple(get_artist(artist["id"]) for artist in related_artists_json)
 
 
 def search_for_artist(search_text: str) -> Artist:
     try:
         return get_artist(search_text)
     except (HTTPError, SpotifyException):
-        search = no_timeout(sp.search)
-        search_result = search(search_text, limit=1, type="artist")
-        return get_artist(search_result["artists"]["items"][0]["id"])
+        search_result = spotify.search_for_artist(search_text)
+        return get_artist(search_result["id"])
 
 
 if __name__ == "__main__":
--- lib/models/spotify.py
+++ lib/models/spotify.py
@@ -9,7 +9,11 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(), retries=None)
 
 _get_album = no_timeout(sp.album)
 _get_album_tracks = no_timeout(sp.album_tracks)
+_get_artist = no_timeout(sp.artist)
+_get_related_artists = no_timeout(sp.artist_related_artists)
+
 _next = no_timeout(sp.next)
+_search = no_timeout(sp.search)
 
 
 @cache
@@ -29,3 +33,17 @@ def get_album_tracks(album_id: str) -> list[dict]:
         response = _next(response)
 
     return tracks
+
+
+@cache
+def get_artist(artist_id: str) -> dict:
+    return _get_artist(artist_id)
+
+
+@cache
+def get_related_artists(artist_id: str) -> list[dict]:
+    return _get_related_artists(artist_id)["artists"]
+
+
+def search_for_artist(search_text: str) -> dict:
+    return _search(search_text, limit=1, type="artist")["artists"]["items"][0]
