342276fdf3a27868673b932a02e8f1b0db8175ad
Mason Albright <14malbright@gmail.com>
2022-12-03 17:13:47 -0600

    Refactor everynoise_scraper.py

--- everynoise_scraper.py
+++ everynoise_scraper.py
@@ -5,23 +5,21 @@ from bs4 import BeautifulSoup
 import cssutils
 import requests
 
-url = "https://everynoise.com/"
-
-response = requests.get(url)
+response = requests.get("https://everynoise.com/")
+response.raise_for_status()
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-genre_elements = soup.find_all("div", class_="genre scanme")
-
-genre_positions = {}
-
-for element in genre_elements:
-    genre_name = element.text[:-2]
-    genre_positions[genre_name] = {}
-
-    style = cssutils.parseStyle(element.attrs["style"])
-    genre_positions[genre_name]["top"] = int(style["top"].replace("px", ""))
-    genre_positions[genre_name]["left"] = int(style["left"].replace("px", ""))
+genre_positions = {
+    element.text[:-2]: {  # genre name
+        "top": int(style["top"].replace("px", "")),
+        "left": int(style["left"].replace("px", "")),
+    }
+    for element in soup.find_all("div", class_="genre scanme")
+    if (style := cssutils.parseStyle(element.attrs["style"]))
+    and "top" in style
+    and "left" in style
+}
 
 with open("genre_positions.json", "w") as f:
     json.dump(genre_positions, f)
