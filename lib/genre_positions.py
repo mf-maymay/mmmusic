0342e9e7ab621419ee0741e9bf0baa049724557e
Mason Albright <14malbright@gmail.com>
2022-12-09 20:52:25 -0600

    Refactor

--- lib/genre_positions.py
+++ lib/genre_positions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from lib.models.artist import get_artist
 from lib.models.genre_attributes import (
+    GenreAttributes,
     get_default_genre_attributes,
     get_genre_attribute_means,
     get_genre_attributes,
@@ -8,7 +9,7 @@ from lib.models.genre_attributes import (
 from lib.models.track import Track
 
 
-def genre_position(track: Track) -> tuple:
+def genre_position(track: Track) -> GenreAttributes:
     genre_positions = get_genre_attributes()
 
     genres = set()
@@ -16,7 +17,6 @@ def genre_position(track: Track) -> tuple:
         genres |= get_artist(artist).genres & genre_positions.keys()
         # NOTE: genres missing from genre_positions are ignored.
 
-    if not genres:
-        return get_default_genre_attributes()
-
-    return get_genre_attribute_means(genres)
+    return (
+        get_genre_attribute_means(genres) if genres else get_default_genre_attributes()
+    )
