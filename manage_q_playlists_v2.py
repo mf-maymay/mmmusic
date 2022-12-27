c92ff991fb22bbda44fcf0dca8d77d24f7256f49
Mason Albright <14malbright@gmail.com>
2022-12-26 19:38:47 -0500

    Remove already-saved tracks

--- manage_q_playlists_v2.py
+++ manage_q_playlists_v2.py
@@ -26,24 +26,43 @@ def remove_tracks_from_playlist(playlist_id, *, tracks, user: User):
 user = User()
 
 # Get playlist tracks
-print("Getting playlist tracks...")
+print("Getting tracks...")
 
-candidates = tracks_from_playlist(CANDIDATES_ID)(user)
+candidates = set(tracks_from_playlist(CANDIDATES_ID)(user))
 
-q_all_tracks = tracks_from_playlist(Q_ALL_ID)(user)
+q_all_tracks = set(tracks_from_playlist(Q_ALL_ID)(user))
 
-rejects = tracks_from_playlist(REJECTS_ID)(user)
+rejects = set(tracks_from_playlist(REJECTS_ID)(user))
+
+user_tracks = set(user.all_tracks())
 
 # Add candidates to 'q - all'
-print("Adding candidates...")
+candidates_to_add = candidates - q_all_tracks
 
-add_tracks_to_playlist(Q_ALL_ID, tracks=set(candidates) - set(q_all_tracks), user=user)
+print(f"Adding {len(candidates_to_add):,} candidates...")
+
+add_tracks_to_playlist(Q_ALL_ID, tracks=candidates_to_add, user=user)
+
+q_all_tracks |= candidates_to_add
 
 clear_playlist(CANDIDATES_ID, user=user)
 
 # Remove rejects from 'q - all'
-print("Removing rejects...")
+rejects_to_remove = rejects & q_all_tracks
 
-remove_tracks_from_playlist(Q_ALL_ID, tracks=rejects, user=user)
+print(f"Removing {len(rejects_to_remove):,} rejects...")
+
+remove_tracks_from_playlist(Q_ALL_ID, tracks=rejects_to_remove, user=user)
+
+q_all_tracks -= rejects_to_remove
 
 clear_playlist(REJECTS_ID, user=user)
+
+# Remove already-saved from 'q - all'
+already_saved_to_remove = q_all_tracks & user_tracks
+
+print(f"Removing {len(already_saved_to_remove):,} already-saved tracks...")
+
+remove_tracks_from_playlist(Q_ALL_ID, tracks=already_saved_to_remove, user=user)
+
+q_all_tracks -= already_saved_to_remove
