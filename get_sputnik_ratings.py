7b53d451aa2331e46e1c3720da0e2bbc4d2135fd
Mason Albright <14malbright@gmail.com>
2023-01-26 09:22:28 -0600

    Use lib.models.sputnik to get ratings

--- get_sputnik_ratings.py
+++ get_sputnik_ratings.py
@@ -1,76 +1,38 @@
-import re
+from collections import defaultdict
 
-from bs4 import BeautifulSoup
 import pandas as pd
-import requests
 
-from lib.models.artist import get_artist
+from lib.models import sputnik
 from lib.user import User
-from lib.utils import no_timeout
-
-valid_rating = re.compile("[0-5]\\.[0-9]")
-valid_vote = re.compile("([0-9]|,)+ Votes")
-
-SEARCH_TEMPLATE = "http://www.sputnikmusic.com/search_results.php?search_text={}"
-
-
-@no_timeout
-def sputnik_rating(artist, album):
-    search = SEARCH_TEMPLATE.format(artist)
-
-    req = requests.get(search)
-
-    soup = BeautifulSoup(req.content, "html.parser")
-
-    album_elem = soup.find("a", text=album)
-
-    if album_elem is None:
-        return None, None  # XXX
-
-    parents = album_elem.parents
-
-    _ = next(parents)
-    _ = next(parents)
-    td = next(parents)
-
-    rating_elem = td.find("b", text=valid_rating)
-
-    votes_elem = td.find("font", text=valid_vote)
-
-    if votes_elem is None:
-        votes = 0
-    else:
-        votes = int(votes_elem.text.split()[0].replace(",", ""))
-
-    if rating_elem is None:
-        return None, None
-
-    try:
-        return float(rating_elem.text), votes
-    except ValueError:
-        return None, None
-
-
-def _artist_album_pair(album):
-    return get_artist(album.artist_ids[0]).name, album.name
 
 
 def get_user_album_ratings(user):
-    ratings_dict = {
-        _artist_album_pair(album): sputnik_rating(*_artist_album_pair(album))
-        for album in user.albums()
+    artist_album_ratings = {  # Artist -> list[SputnikAlbum]
+        artist: sputnik.get_artist_albums(artist) for artist in user.artists()
     }
 
-    ratings = [
-        (artist, album, rating, votes)
-        for (artist, album), (rating, votes) in ratings_dict.items()
-        if rating is not None
-    ]
+    artist_saved_albums = defaultdict(set)  # artist_id (str) -> set[Album]
+    for album in user.albums():
+        for artist_id in album.artist_ids:
+            artist_saved_albums[artist_id].add(album)
 
-    return pd.DataFrame(ratings, columns=["Artist", "Album", "Rating", "Votes"])
+    saved_album_ratings = [
+        album
+        for artist, albums in artist_album_ratings.items()
+        for album in albums
+        if artist.id in artist_saved_albums
+        and any(
+            album["name"] == saved_album.name
+            for saved_album in artist_saved_albums[artist.id]
+        )
+    ]  # NOTE: artist is assumed to be an Artist, here
+
+    return pd.DataFrame(saved_album_ratings)
 
 
 if __name__ == "__main__":
     user = User()
 
     ratings = get_user_album_ratings(user)
+
+    print(ratings)
