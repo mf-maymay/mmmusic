588106c083c956d30455e84bde475164898f0221
Mason Albright <14malbright@gmail.com>
2023-01-29 22:58:20 -0600

    Add get_metrics_for_album

--- lib/features.py
+++ lib/features.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from lib.genres import get_track_genre_attributes
-from lib.models.album import get_album
+from lib.models.album import Album, get_album, get_album_tracks
 from lib.models.track import Track
 
 Metrics = list[float]
@@ -30,3 +30,15 @@ def get_metrics_for_track(track: Track) -> Metrics:
         int(get_album(track.album_id).release_date.year),
         *get_track_genre_attributes(track),
     ]
+
+
+def get_metrics_for_album(album: Album) -> Metrics:
+    return np.mean(
+        [get_metrics_for_track(track) for track in get_album_tracks(album)], axis=0
+    )
+
+
+if __name__ == "__main__":
+    album = get_album("2Ul7B1LEHxXzYubtkTMENs")
+
+    album_metrics = get_metrics_for_album(album)
