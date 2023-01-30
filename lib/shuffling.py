86d108ce9a069beac412b8af5c3a14be29fd1617
Mason Albright <14malbright@gmail.com>
2023-01-29 19:26:57 -0600

    Remove balanced and smart modes

--- lib/shuffling.py
+++ lib/shuffling.py
@@ -87,28 +87,6 @@ def _get_average_values(
     }
 
 
-def _get_categorical_scores(
-    left: Item, right: Item, to_add: Item
-) -> dict[str, Metrics]:
-    # key, mode, artist
-    left_values = [
-        sum(track["key"] == to_add["key"] for track in left),
-        sum(track["mode"] == to_add["mode"] for track in left),
-        sum(track.artist_ids[0] == to_add.artist_ids[0] for track in left),
-    ]
-    right_values = [
-        sum(track["key"] == to_add["key"] for track in right),
-        sum(track["mode"] == to_add["mode"] for track in right),
-        sum(track.artist_ids[0] == to_add.artist_ids[0] for track in right),
-    ]
-    return {
-        "left": [x / len(left) for x in left_values],
-        "left with new": [(x + 1) / (len(left) + 1) for x in left_values],
-        "right": [x / len(right) for x in right_values],
-        "right with new": [(x + 1) / (len(right) + 1) for x in right_values],
-    }
-
-
 def _scores(
     items: Items, metrics_func: Callable[[Item], Metrics]
 ) -> dict[Item, Metrics]:
@@ -118,32 +96,6 @@ def _scores(
     for j, col in enumerate(metrics.T):
         scores[:, j] = [percentileofscore(col, x, kind="mean") for x in col]
     return dict(zip(items, scores))
-
-
-def _balanced_metrics(track: Track) -> Metrics:
-    return [track[metric] for metric in METRICS] + [
-        track["duration_ms"],
-        int(get_album(track.album_id).release_date.year),
-        *get_track_genre_attributes(track),
-    ]
-
-
-def _balanced_picker(tracks: Tracks) -> ItemPicker:
-    values = _scores(tracks, _balanced_metrics)
-
-    def picker(left: Tracks, right: Tracks, to_add: Track, items: Tracks) -> bool:
-        # add item to left if diff less with item in left than in right
-        averages = _get_average_values(left, right, to_add, values)
-        scores = _get_categorical_scores(left, right, to_add)
-        return similarity(
-            np.append(averages["left with new"], scores["left with new"]),
-            np.append(averages["right"], scores["right"]),
-        ) > similarity(
-            np.append(averages["left"], scores["left"]),
-            np.append(averages["right with new"], scores["right with new"]),
-        )
-
-    return picker
 
 
 def _story_metrics(track: Track) -> Metrics:
@@ -174,22 +126,6 @@ def _artists_of_tracks(tracks: Tracks) -> dict[Track, int]:
     return artists
 
 
-def _smart_picker(tracks: Tracks) -> ItemPicker:
-    balanced_picker = _balanced_picker(tracks)
-    story_picker = _story_picker(tracks)
-
-    def picker(left: Tracks, right: Tracks, to_add: Track, items: Tracks) -> bool:
-        artist_counts = _artists_of_tracks(items)
-
-        # If any tracks share artists, use balanced picker.
-        if any(count > 1 for count in artist_counts.values()):
-            return balanced_picker(left, right, to_add, items)
-        # If all tracks have unique artists, use story picker.
-        return story_picker(left, right, to_add, items)
-
-    return picker
-
-
 def _radio_picker(tracks: Tracks) -> ItemPicker:
     story_picker = _story_picker(tracks)
 
@@ -208,6 +144,7 @@ def _radio_picker(tracks: Tracks) -> ItemPicker:
         # If one side has fewer of to_add's artists, add to that side.
         if num_mutual_artists_in_left != num_mutual_artists_in_right:
             return num_mutual_artists_in_left < num_mutual_artists_in_right
+
         # Otherwise, use story mode.
         return story_picker(left, right, to_add, items)
 
@@ -218,7 +155,6 @@ def _smart_seed_picker(tracks: Tracks) -> SeedPicker:
     values = _scores(tracks, _story_metrics)
 
     def picker(items: Tracks, left_neighbor: Optional[Track]) -> Tuple[Track, Track]:
-
         if left_neighbor is None:
             left_seed = items[0]
         else:
@@ -227,6 +163,7 @@ def _smart_seed_picker(tracks: Tracks) -> SeedPicker:
             left_seed = max(
                 items, key=lambda item: similarity(neighbor_value, values[item])
             )
+
         # Pick item least similar to left seed
         left_value = values[left_seed]
         right_seed = min(items, key=lambda item: similarity(left_value, values[item]))
@@ -241,13 +178,9 @@ def smart_shuffle(tracks: Tracks, mode: str = "radio"):
 
     seed_picker = None
 
-    if mode == "balanced":
-        picker = _balanced_picker(tracks)
-    elif mode == "radio":
+    if mode == "radio":
         picker = _radio_picker(tracks)
         seed_picker = _smart_seed_picker(tracks)
-    elif mode == "smart":
-        picker = _smart_picker(tracks)
     elif mode == "story":
         picker = _story_picker(tracks)
     else:
