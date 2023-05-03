ff73e2c37ee8c9af8a93e5850e121db0dbb32740
Mason Albright <14malbright@gmail.com>
2023-05-02 22:08:39 -0500

    Add script for making Built To Spill playlist

---
+++ built_to_spill.py
@@ -0,0 +1,23 @@
+from functools import partial
+
+from lib.playlist import Playlist
+from lib.playlist_management import get_tracks_from_playlist
+from lib.shuffling import smart_shuffle, _story_picker
+from lib.user import User
+
+
+def track_source(user):
+    return get_tracks_from_playlist("0WXuGXw1OSkSct4aYW4Nnu", user=user)
+
+
+user = User()
+
+playlist = Playlist(
+    "Building Up Built To Spill",
+    track_source=track_source,
+    order_tracks_func=partial(smart_shuffle, picker_factory=_story_picker),
+)
+
+playlist.get_tracks(user)
+playlist.order_tracks()
+playlist.create(user)
