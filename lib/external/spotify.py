3259e40e0da5e60d912f9194a87cae4cc8940935
Mason Albright <14malbright@gmail.com>
2023-05-24 22:23:53 -0500

    Move private functions to where they're used

--- lib/external/spotify.py
+++ lib/external/spotify.py
@@ -11,25 +11,17 @@ def get_client_credentials_managed_client() -> spotipy.Spotify:
     return spotipy.Spotify(auth_manager=SpotifyClientCredentials(), retries=None)
 
 
-_get_album = no_timeout(get_client_credentials_managed_client().album)
-_get_album_tracks = no_timeout(get_client_credentials_managed_client().album_tracks)
-_get_artist = no_timeout(get_client_credentials_managed_client().artist)
-_get_artist_related_artists = no_timeout(
-    get_client_credentials_managed_client().artist_related_artists
-)
-_get_track = no_timeout(get_client_credentials_managed_client().track)
-_get_track_audio_features = no_timeout(
-    get_client_credentials_managed_client().audio_features
-)
 _next = no_timeout(get_client_credentials_managed_client().next)
-_search = no_timeout(get_client_credentials_managed_client().search)
 
 
 def get_album(album_id: str) -> dict:
+    _get_album = no_timeout(get_client_credentials_managed_client().album)
     return _get_album(album_id)
 
 
 def get_album_tracks(album_id: str) -> list[dict]:
+    _get_album_tracks = no_timeout(get_client_credentials_managed_client().album_tracks)
+
     tracks = []
 
     response = _get_album_tracks(album_id)
@@ -43,20 +35,29 @@ def get_album_tracks(album_id: str) -> list[dict]:
 
 
 def get_artist(artist_id: str) -> dict:
+    _get_artist = no_timeout(get_client_credentials_managed_client().artist)
     return _get_artist(artist_id)
 
 
 def get_artist_related_artists(artist_id: str) -> list[dict]:
+    _get_artist_related_artists = no_timeout(
+        get_client_credentials_managed_client().artist_related_artists
+    )
     return _get_artist_related_artists(artist_id)["artists"]
 
 
 def get_track(track_id: str) -> dict:
+    _get_track = no_timeout(get_client_credentials_managed_client().track)
     return _get_track(track_id)
 
 
 def get_track_audio_features(track_id: str) -> dict:
+    _get_track_audio_features = no_timeout(
+        get_client_credentials_managed_client().audio_features
+    )
     return _get_track_audio_features(track_id)[0]
 
 
 def search_for_artist(search_text: str) -> dict:
+    _search = no_timeout(get_client_credentials_managed_client().search)
     return _search(search_text, limit=1, type="artist")["artists"]["items"][0]
