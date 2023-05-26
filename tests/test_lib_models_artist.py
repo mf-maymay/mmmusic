412d6cb7a9fa42d5dbaafed2dc37d1ef00cf2c4e
Mason Albright <14malbright@gmail.com>
2023-05-25 20:44:38 -0500

    Make test make sense
    Changed mocked `spotify.get_artist` return correct type.

--- tests/test_lib_models_artist.py
+++ tests/test_lib_models_artist.py
@@ -9,18 +9,26 @@ class TestArtist(unittest.TestCase):
     def test_get_artist(self, mock_spotify):
         fake_artist_id = "fake_artist_id"
 
-        mock_spotify.get_artist.return_value = fake_artist = Artist(
-            name="fake_name",
-            id=fake_artist_id,
-            genres=("fake_genre",),
-            popularity=50,
-        )
+        mock_spotify.get_artist.return_value = {
+            "name": "fake_name",
+            "id": fake_artist_id,
+            "genres": ("fake_genre",),
+            "popularity": 50,
+        }
 
         artist = get_artist(fake_artist_id)
 
         mock_spotify.get_artist.assert_called_once_with(fake_artist_id)
 
-        self.assertEqual(artist, fake_artist)
+        self.assertEqual(
+            artist,
+            Artist(
+                name="fake_name",
+                id=fake_artist_id,
+                genres=("fake_genre",),
+                popularity=50,
+            ),
+        )
 
     @patch("lib.models.artist.spotify", autospec=True)
     def test_get_artist_returns_artist_as_is_when_supplied_artist(self, mock_spotify):
