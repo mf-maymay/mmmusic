a3b74857eec40c6f252ab9149f16321a7404b323
Mason Albright <14malbright@gmail.com>
2023-06-24 09:34:20 -0500

    Include `track_source` in config

--- lib/models/playlist_configs.py
+++ lib/models/playlist_configs.py
@@ -1,12 +1,15 @@
+from typing import Callable
+
 import pydantic
 
-from lib.models.types import TrackListTransformer
+from lib.models.types import TrackListTransformer, Tracks
 from lib.shuffling import smart_shuffle
 
 
 class PlaylistConfig(pydantic.BaseModel):
-    id: str | None = None
+    id: str | None
     name: str
     description: str = ""
+    track_source: Callable[[], Tracks] | None
     order_tracks_func: TrackListTransformer = smart_shuffle
     track_filters: list[TrackListTransformer] = []
