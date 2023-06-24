5b915c9c1431b346b00d8dc8618eeb01e315ada0
Mason Albright <14malbright@gmail.com>
2023-06-24 12:44:59 -0500

    Reference attributes of config

--- lib/playlists/generated_playlists.py
+++ lib/playlists/generated_playlists.py
@@ -21,37 +21,38 @@ class GeneratedPlaylist:
 
         self.id = config.id
 
-        self.name = config.name
-        self.description = config.description
-
         self.track_source = (
             config.track_source
             if config.track_source is not None
             else user.get_tracks_from_saved_albums
         )
-        self.track_filters = config.track_filters
-        self.order_tracks_func = config.order_tracks_func
 
         self.tracks = None
+
+    @property
+    def name(self) -> str:
+        return self.config.name
 
     def get_tracks(self):
         self.tracks = self.track_source()
 
-        for track_filter in self.track_filters:
+        for track_filter in self.config.track_filters:
             self.tracks = track_filter(self.tracks)
 
     def order_tracks(self):
         if self.tracks is None:
             raise ValueError("tracks not set")
 
-        self.tracks = self.order_tracks_func(self.tracks)
+        self.tracks = self.config.order_tracks_func(self.tracks)
 
     def create(self):
         if self.id is not None:
             raise ValueError("id is already set")
 
         self.id = create_playlist(
-            name=self.name, description=self.description, user=self.user
+            name=self.config.name,
+            description=self.config.description,
+            user=self.user,
         )
 
         add_tracks_to_playlist(self.id, tracks=self.tracks, user=self.user)
