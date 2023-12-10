cd5d784091aa60d32e22b26cc96be21e5ba721f5
Mason Albright <14malbright@gmail.com>
2023-12-10 13:58:46 -0600

    Use computed_field for combined_processor

--- mmmusic/models/playlist_configs.py
+++ mmmusic/models/playlist_configs.py
@@ -1,29 +1,28 @@
-from functools import reduce
+from functools import cached_property, reduce
 from operator import and_
 
-import pydantic
+from pydantic import BaseModel, computed_field, model_validator
 
 from mmmusic.models.types import TrackListTransformer
 from mmmusic.track_sources import TrackSource, from_saved_albums
 
 
-class PlaylistConfig(pydantic.BaseModel, arbitrary_types_allowed=True, extra="forbid"):
+class PlaylistConfig(BaseModel, arbitrary_types_allowed=True, extra="forbid"):
     id: str | None = None
     name: str
     description: str | None = None
     track_source: TrackSource = from_saved_albums
     processors: list[TrackListTransformer] = []
 
-    _combined_processor: TrackListTransformer | None = None
-
-    @property
+    @computed_field
+    @cached_property
     def combined_processor(self) -> TrackListTransformer | None:
-        if self._combined_processor is None and self.processors:
-            self._combined_processor = reduce(and_, self.processors)
+        if not self.processors:
+            return None
 
-        return self._combined_processor
+        return reduce(and_, self.processors)
 
-    @pydantic.model_validator(mode="after")
+    @model_validator(mode="after")
     def fill_in_description(self):
         if self.description is None:
             if self.combined_processor is None:
