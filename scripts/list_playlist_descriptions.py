9bfa62a9652ec21a66850ae9273da131496719e4
Mason Albright <14malbright@gmail.com>
2024-12-02 20:06:34 -0600

    Remove scripts

--- scripts/list_playlist_descriptions.py
+++
@@ -1,4 +0,0 @@
-from mmmusic.data.playlist_configs import regular_playlists
-
-for playlist_config in regular_playlists:
-    print(f"{playlist_config.name}: {playlist_config.description}")  # noqa: T201
